9de681b4d40b1cb5b8c37d1e523adfe14663b316
Ali Can Yücel <yucelalican30@gmail.com>
2023-09-21 19:17:35 +0300

    OpenClosedPrincipleNet7.0
    OpenClosedPrincipleNet7.0

---
+++ OpenClosedPrinciple/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace OpenClosedPrinciple;
+class ParaGonderici
+{
+    public void Gonder(IBanka banka, int tutar, string hesapNo)
+    {
+        banka.ParaTransferi(tutar, hesapNo);
+    }
+}
+
+interface IBanka
+{
+    bool ParaTransferi(int tutar, string hesapNo);
+}
+class Garanti : IBanka
+{
+    public string HesapNo { get; set; }
+    public void ParaGonder(int tutar)
+    {
+        //...
+    }
+
+    public bool ParaTransferi(int tutar, string hesapNo)
+    {
+        try
+        {
+
+            HesapNo = hesapNo;
+            ParaGonder(tutar);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+class Halkbank : IBanka
+{
+    string _hesapNo;
+    public void GonderilecekHesapNo(string hesapNo)
+    {
+        //...
+    }
+    public void Gonder(int tutar)
+    {
+        //...
+    }
+
+    public bool ParaTransferi(int tutar, string hesapNo)
+    {
+        try
+        {
+            GonderilecekHesapNo(hesapNo);
+            Gonder(tutar);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+class AliCanBank : IBanka
+{
+    public bool ParaTransferi(int tutar, string hesapNo)
+    {
+        throw new NotImplementedException();
+    }
+}
